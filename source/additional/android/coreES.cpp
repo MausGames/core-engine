2db6b9686fb8fa74e532ace3d5ba069bfab2f392
Martin Mauersics <revision@maus-games.at>
2014-06-16 01:57:18 +0200

    updated OpenGL ES handling files, implemented texture storage and anisotropic filter

--- source/additional/android/coreES.cpp
+++ source/additional/android/coreES.cpp
@@ -6,23 +6,52 @@
 //| More information available in the readme file      |//
 //*----------------------------------------------------*//
 //////////////////////////////////////////////////////////
-/* only required for OpenGL ES 2.0 applications,
- * implements unavailable definitions and dummy-functions */
-
 #include "Core.h"
 #include <EGL/egl.h>
 
-
-std::string g_sExtensions = "";
-bool m_bGL_EXT_discard_framebuffer = false;
+std::string g_sExtensions                 = "";
+bool m_bGL_EXT_discard_framebuffer        = false;
+bool m_bGL_EXT_texture_storage            = false;
+bool m_bGL_EXT_texture_filter_anisotropic = false;
 
 PFNGLDISCARDFRAMEBUFFEREXTPROC glDiscardFramebufferEXT = NULL;
+PFNGLTEXSTORAGE2DEXT           glTexStorage2DEXT       = NULL;
+GLenum                         GL_RGBA8                = GL_RGBA;
+GLenum                         GL_RGB8                 = GL_RGB;
 
-GLenum glewInit()
+
+// ****************************************************************  
+/* check for implemented extensions */
+int coreInitES()
 {
+    // get and save full extension string
     g_sExtensions = r_cast<const char*>(glGetString(GL_EXTENSIONS));
-    m_bGL_EXT_discard_framebuffer = (g_sExtensions.find("GL_EXT_discard_framebuffer") != std::string::npos);
-    if(m_bGL_EXT_discard_framebuffer) glDiscardFramebufferEXT = (PFNGLDISCARDFRAMEBUFFEREXTPROC)eglGetProcAddress("glDiscardFramebufferEXT");
-    
-    return GLEW_OK;
+
+    // check for GL_EXT_discard_framebuffer
+    if((m_bGL_EXT_discard_framebuffer = __CORE_ES_CHECK("GL_EXT_discard_framebuffer")))
+    {
+        // fetch required function
+        __CORE_ES_FETCH(PFNGLDISCARDFRAMEBUFFEREXTPROC, glDiscardFramebufferEXT)
+    }
+
+    // check for GL_EXT_texture_storage
+    if((m_bGL_EXT_texture_storage = __CORE_ES_CHECK("GL_EXT_texture_storage") && __CORE_ES_CHECK("GL_OES_rgb8_rgba8")))
+    {
+        // fetch required function
+        __CORE_ES_FETCH(PFNGLTEXSTORAGE2DEXT, glTexStorage2DEXT)
+
+        // adjust texture format parameters
+        GL_RGBA8 = GL_RGBA8_OES;
+        GL_RGB8  = GL_RGB8_OES;
+    }
+
+    // check for GL_EXT_texture_filter_anisotropic
+    m_bGL_EXT_texture_filter_anisotropic = __CORE_ES_CHECK("GL_EXT_texture_filter_anisotropic");
+
+    // write extension status to log
+    Core::Log->Info("GL_EXT_discard_framebuffer:        %d (%d)", m_bGL_EXT_discard_framebuffer        ? 1 : 0, glDiscardFramebufferEXT);
+    Core::Log->Info("GL_EXT_texture_storage:            %d (%d)", m_bGL_EXT_texture_storage            ? 1 : 0, glTexStorage2DEXT);
+    Core::Log->Info("GL_EXT_texture_filter_anisotropic: %d",      m_bGL_EXT_texture_filter_anisotropic ? 1 : 0);
+
+    return 0;
 }
--- source/additional/android/coreES.h
+++ source/additional/android/coreES.h
@@ -6,139 +6,140 @@
 //| More information available in the readme file      |//
 //*----------------------------------------------------*//
 //////////////////////////////////////////////////////////
-/* only required for OpenGL ES 2.0 applications,
- * implements unavailable definitions and dummy-functions */
 #pragma once
-#ifndef _GLEWES_H_
-#define	_GLEWES_H_
+#ifndef _CORE_ES_H_
+#define	_CORE_ES_H_
 
 #ifdef __cplusplus
-extern "C"
-{
+    extern "C" {
 #endif
 
 
-#define GLEW_OK 0
-
-#define GLEW_VERSION 1
-
-#define GL_DEBUG_OUTPUT 0x92E0
-
-#define GL_UNIFORM_BUFFER 0x8A11
-#define GL_PIXEL_UNPACK_BUFFER 0x88EC
-
-#define GL_MAP_WRITE_BIT 0x0002
-#define GL_MAP_INVALIDATE_RANGE_BIT 0x0004
-#define GL_MAP_INVALIDATE_BUFFER_BIT 0x0008
-#define GL_MAP_UNSYNCHRONIZED_BIT 0x0020
-
-#define GL_STATIC_READ 0x88E5
-#define GL_STATIC_COPY 0x88E6
-
-#define GL_WRITE_ONLY 0x88B9
-
-#define GL_TEXTURE_MAX_ANISOTROPY_EXT 0x84FE
-
-#define GL_SYNC_FLUSH_COMMANDS_BIT 0x00000001
-#define GL_SYNC_GPU_COMMANDS_COMPLETE 0x9117
-#define GL_TIMEOUT_EXPIRED 0x911B
-
-#define GL_RGBA8 GL_RGBA
-#define GL_RGB8  GL_RGB
+// ****************************************************************   
+/* default extensions */
+#define GLEW_VERSION_3_2               false
+#define GLEW_ARB_map_buffer_range      false
+#define GLEW_ARB_buffer_storage        false
+#define GLEW_ARB_clear_buffer_object   false
+#define GLEW_ARB_vertex_attrib_binding false
+#define GLEW_ARB_uniform_buffer_object false
+#define GLEW_ARB_vertex_array_object   false
+#define GLEW_ARB_tessellation_shader   false
+#define GLEW_ARB_sync                  false
+#define GLEW_ARB_pixel_buffer_object   false
+#define GLEW_ARB_instanced_arrays      false
+#define GLEW_ARB_debug_output          false
+#define GLEW_KHR_debug                 false
+#define GLEW_ARB_framebuffer_object    true
 
 
-#define GL_GEOMETRY_SHADER 0x8DD9
-#define GL_TESS_EVALUATION_SHADER 0x8E87
-#define GL_TESS_CONTROL_SHADER 0x8E88
-
-
-typedef int* GLsync;
-typedef uint64_t GLuint64;
-
-
+// ****************************************************************   
+/* handle implemented extensions */
 extern std::string g_sExtensions;
 extern bool m_bGL_EXT_discard_framebuffer;
-extern GLenum glewInit();
+extern bool m_bGL_EXT_texture_storage;
+extern bool m_bGL_EXT_texture_filter_anisotropic;
+
+extern int coreInitES();
+
+#define __CORE_ES_CHECK(x)   (g_sExtensions.find(x) != std::string::npos)
+#define __CORE_ES_FETCH(t,x) {x = (t)eglGetProcAddress(#x);}
 
 
-static const char* glewGetString(GLenum name)       {return "";}
-static const char* glewGetErrorString(GLenum error) {return "";}
+// ****************************************************************   
+/* implemented GL_EXT_discard_framebuffer (mapped on GL_ARB_invalidate_subdata) */
+#define GLEW_ARB_invalidate_subdata m_bGL_EXT_discard_framebuffer
 
-#define GLEW_ARB_map_buffer_range           false
-#define GLEW_ARB_buffer_storage             false
-#define GLEW_ARB_clear_buffer_object        false
-#define GLEW_ARB_vertex_attrib_binding      false
-#define GLEW_ARB_texture_storage            false
-#define GLEW_ARB_framebuffer_object         true
-#define GLEW_ARB_uniform_buffer_object      false
-#define GLEW_ARB_vertex_array_object        false
-#define GLEW_ARB_tessellation_shader        false
-#define GLEW_ARB_sync                       false
-#define GLEW_ARB_pixel_buffer_object        false
-#define GLEW_ARB_instanced_arrays           false
-#define GLEW_ARB_debug_output               false
-#define GLEW_EXT_texture_filter_anisotropic false
-#define GLEW_KHR_debug                      false
-#define GLEW_VERSION_3_2                    false
-
-
-// GL_EXT_discard_framebuffer
-#define GLEW_ARB_invalidate_subdata         m_bGL_EXT_discard_framebuffer
 #define GL_DEPTH GL_DEPTH_EXT
 #define glInvalidateBufferData(x)
 #define glInvalidateFramebuffer glDiscardFramebufferEXT
 extern PFNGLDISCARDFRAMEBUFFEREXTPROC glDiscardFramebufferEXT;
 
 
-#define glClearDepth glClearDepthf
-#define glDrawRangeElements(a, b, c, d, e, f) glDrawElements(a, d, e, f)
+// ****************************************************************   
+/* implemented GL_EXT_texture_storage (mapped on GL_ARB_texture_storage) */
+#define GLEW_ARB_texture_storage m_bGL_EXT_texture_storage
+
+#define glTexStorage2D glTexStorage2DEXT
+typedef void (GL_APIENTRYP PFNGLTEXSTORAGE2DEXT) (GLenum target, GLsizei levels, GLenum internalformat, GLsizei width, GLsizei height);
+extern PFNGLTEXSTORAGE2DEXT glTexStorage2DEXT;
+extern GLenum GL_RGBA8;
+extern GLenum GL_RGB8;
 
 
+// ****************************************************************   
+/* implemented GL_EXT_texture_filter_anisotropic */
+#define GLEW_EXT_texture_filter_anisotropic m_bGL_EXT_texture_filter_anisotropic
 
 
-typedef void (*GLDEBUGPROC)(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, GLvoid* userParam);
-static void glDebugMessageCallback(GLDEBUGPROC callback, const GLvoid *userParam)                                                   {}
-static void glDebugMessageControl(GLenum source, GLenum type, GLenum severity, GLsizei count, const GLuint* ids, GLboolean enabled) {}
-static void glDebugMessageCallbackARB(GLDEBUGPROC callback, const GLvoid *userParam)                                                   {}
-static void glDebugMessageControlARB(GLenum source, GLenum type, GLenum severity, GLsizei count, const GLuint* ids, GLboolean enabled) {}
+// ****************************************************************   
+/* other rerouted functions */
+#define glewInit                         coreInitES
+#define glClearDepth                     glClearDepthf
+#define glDrawRangeElements(a,b,c,d,e,f) glDrawElements(a, d, e, f)
 
-static void glBindVertexArray(GLuint array)                       {}
-static void glDeleteVertexArrays(GLsizei n, const GLuint* arrays) {}
-static void glGenVertexArrays(GLsizei n, GLuint* arrays)          {}
 
-static void glBindBufferBase(GLenum target, GLuint index, GLuint buffer) {}
-static void glBindFragDataLocation(GLuint, GLuint, const GLchar*)        {}
+// ****************************************************************  
+/* unused types, definitions and functions */
+#define GLEW_OK      0
+#define GLEW_VERSION 1          
+static const char* glewGetString(GLenum name)       {return "";}
+static const char* glewGetErrorString(GLenum error) {return "";}
 
-static GLuint glGetUniformBlockIndex(GLuint program, const GLchar* uniformBlockName)                    {return -1;}
-static void glUniformBlockBinding(GLuint program, GLuint uniformBlockIndex, GLuint uniformBlockBinding) {}
+typedef int*     GLsync;
+typedef uint64_t GLuint64;
+typedef void (*GLDEBUGPROC) (GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, GLvoid* userParam);
 
-static GLvoid* glMapBuffer(GLenum target, GLenum access)                                            {return NULL;}
-static void* glMapBufferRange(GLenum target, GLintptr offset, GLsizeiptr length, GLbitfield access) {return NULL;}
-static bool glUnmapBuffer(GLenum target)                                                            {return true;}
+#define GL_DEBUG_OUTPUT                 0x92E0
+#define GL_DEBUG_OUTPUT_SYNCHRONOUS     0x8242
+#define GL_DEBUG_OUTPUT_SYNCHRONOUS_ARB 0x8242
+#define GL_UNIFORM_BUFFER               0x8A11
+#define GL_PIXEL_UNPACK_BUFFER          0x88EC
+#define GL_MAP_WRITE_BIT                0x0002
+#define GL_MAP_INVALIDATE_RANGE_BIT     0x0004
+#define GL_MAP_INVALIDATE_BUFFER_BIT    0x0008
+#define GL_MAP_UNSYNCHRONIZED_BIT       0x0020
+#define GL_STATIC_READ                  0x88E5
+#define GL_STATIC_COPY                  0x88E6
+#define GL_WRITE_ONLY                   0x88B9
+#define GL_SYNC_FLUSH_COMMANDS_BIT      0x00000001
+#define GL_SYNC_GPU_COMMANDS_COMPLETE   0x9117
+#define GL_TIMEOUT_EXPIRED              0x911B
+#define GL_GEOMETRY_SHADER              0x8DD9
+#define GL_TESS_EVALUATION_SHADER       0x8E87
+#define GL_TESS_CONTROL_SHADER          0x8E88
 
-static void glBufferStorage(GLenum target, GLsizeiptr size, const GLvoid* data, GLbitfield flags) {}
-
-static void glBindVertexBuffer(GLuint bindingindex, GLuint buffer, GLintptr offset, GLsizei stride) {}
-static void glVertexAttribBinding(GLuint attribindex, GLuint bindingindex) {}
-static void glVertexAttribFormat(GLuint attribindex, GLint size, GLenum type, GLboolean normalized, GLuint relativeoffset) {}
-static void glVertexAttribIFormat(GLuint attribindex, GLint size, GLenum type, GLuint relativeoffset) {}
-
-static void glVertexAttribIPointer(GLuint, GLint, GLenum, GLsizei, const GLvoid*) {}
-static void glVertexBindingDivisor(GLuint bindingindex, GLuint divisor) {}
-static void glVertexAttribDivisorARB(GLuint index, GLuint divisor) {}
-
-static void glTexStorage2D(GLenum target, GLsizei levels, GLenum internalformat, GLsizei width, GLsizei height) {}
-
-static void glDrawArraysInstanced(GLenum, GLint, GLsizei, GLsizei) {}
-static void glDrawElementsInstanced(GLenum, GLsizei, GLenum, const GLvoid*, GLsizei) {}
-
-static GLenum glClientWaitSync(GLsync GLsync,GLbitfield flags,GLuint64 timeout) {return 0;}
-static void glDeleteSync(GLsync GLsync)                                         {}
-static GLsync glFenceSync(GLenum condition,GLbitfield flags)                    {return NULL;}
+static void    glDebugMessageCallback(GLDEBUGPROC callback, const GLvoid *userParam)                                                      {}
+static void    glDebugMessageControl(GLenum source, GLenum type, GLenum severity, GLsizei count, const GLuint* ids, GLboolean enabled)    {}
+static void    glDebugMessageCallbackARB(GLDEBUGPROC callback, const GLvoid *userParam)                                                   {}
+static void    glDebugMessageControlARB(GLenum source, GLenum type, GLenum severity, GLsizei count, const GLuint* ids, GLboolean enabled) {}
+static void    glBindVertexArray(GLuint array)                                                                                            {}
+static void    glDeleteVertexArrays(GLsizei n, const GLuint* arrays)                                                                      {}
+static void    glGenVertexArrays(GLsizei n, GLuint* arrays)                                                                               {}
+static void    glBindBufferBase(GLenum target, GLuint index, GLuint buffer)                                                               {}
+static void    glBindFragDataLocation(GLuint program, GLuint colorNumber, const GLchar* name)                                             {}
+static GLuint  glGetUniformBlockIndex(GLuint program, const GLchar* uniformBlockName)                                                     {return -1;}
+static void    glUniformBlockBinding(GLuint program, GLuint uniformBlockIndex, GLuint uniformBlockBinding)                                {}
+static GLvoid* glMapBuffer(GLenum target, GLenum access)                                                                                  {return NULL;}
+static GLvoid* glMapBufferRange(GLenum target, GLintptr offset, GLsizeiptr length, GLbitfield access)                                     {return NULL;}
+static bool    glUnmapBuffer(GLenum target)                                                                                               {return true;}
+static void    glBufferStorage(GLenum target, GLsizeiptr size, const GLvoid* data, GLbitfield flags)                                      {}
+static void    glBindVertexBuffer(GLuint bindingindex, GLuint buffer, GLintptr offset, GLsizei stride)                                    {}
+static void    glVertexAttribBinding(GLuint attribindex, GLuint bindingindex)                                                             {}
+static void    glVertexAttribFormat(GLuint attribindex, GLint size, GLenum type, GLboolean normalized, GLuint relativeoffset)             {}
+static void    glVertexAttribIFormat(GLuint attribindex, GLint size, GLenum type, GLuint relativeoffset)                                  {}
+static void    glVertexAttribIPointer(GLuint index, GLint size, GLenum type, GLsizei stride, const GLvoid* pointer)                       {}
+static void    glVertexBindingDivisor(GLuint bindingindex, GLuint divisor)                                                                {}
+static void    glVertexAttribDivisorARB(GLuint index, GLuint divisor)                                                                     {}
+static void    glDrawArraysInstanced(GLenum, GLint, GLsizei, GLsizei)                                                                     {}
+static void    glDrawElementsInstanced(GLenum mode, GLsizei count, GLenum type, const GLvoid* indices, GLsizei primcount)                 {}
+static GLenum  glClientWaitSync(GLsync GLsync, GLbitfield flags, GLuint64 timeout)                                                        {return 0;}
+static void    glDeleteSync(GLsync GLsync)                                                                                                {}
+static GLsync  glFenceSync(GLenum condition, GLbitfield flags)                                                                            {return NULL;}
 
 
 #ifdef __cplusplus
-}
+    }
 #endif
 
-#endif // _GLEWES_H_
+#endif /* _CORE_ES_H_ */
