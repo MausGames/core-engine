763a7aa71c2b1e51d6c84b0b7707988a0f27cbca
Martin Mauersics <revision@maus-games.at>
2020-02-04 01:14:51 +0100

    implemented 64-bit Murmur hash function

--- source/utilities/data/hash/Murmur2.h
+++ source/utilities/data/hash/Murmur2.h
@@ -38,15 +38,60 @@ constexpr FUNC_LOCAL coreUint32 coreHashMurmur2(const coreByte* pData, coreUint3
 
     switch(iLength)
     {
-    case 3u: iHash ^= pData[2] << 16u; FALLTHROUGH
-    case 2u: iHash ^= pData[1] << 8u;  FALLTHROUGH
-    case 1u: iHash ^= pData[0];
+    case 3u: iHash ^= coreUint32(pData[2]) << 16u; FALLTHROUGH
+    case 2u: iHash ^= coreUint32(pData[1]) << 8u;  FALLTHROUGH
+    case 1u: iHash ^= coreUint32(pData[0]);
              iHash *= m;
     };
 
     iHash ^= iHash >> 13u;
     iHash *= m;
     iHash ^= iHash >> 15u;
+
+    return iHash;
+}
+
+
+// ****************************************************************
+/* Murmur2 64-bit hash function */
+constexpr FUNC_LOCAL coreUint64 coreHashMurmur64A(const coreByte* pData, coreUint64 iLength)
+{
+    constexpr coreUint64 s = 0xC58F1A7BC58F1A7Bu;
+    constexpr coreUint64 m = 0xC6A4A7935BD1E995u;
+    constexpr coreUint64 r = 47u;
+
+    coreUint64 iHash = s ^ (iLength * m);
+
+    while(iLength >= 8u)
+    {
+      coreUint64 k = (*r_cast<const coreUint64*>(pData));
+
+      k *= m;
+      k ^= k >> r;
+      k *= m;
+
+      iHash ^= k;
+      iHash *= m;
+
+      pData   += 8u;
+      iLength -= 8u;
+    }
+
+    switch(iLength)
+    {
+    case 7u: iHash ^= coreUint64(pData[6]) << 48u; FALLTHROUGH
+    case 6u: iHash ^= coreUint64(pData[5]) << 40u; FALLTHROUGH
+    case 5u: iHash ^= coreUint64(pData[4]) << 32u; FALLTHROUGH
+    case 4u: iHash ^= coreUint64(pData[3]) << 24u; FALLTHROUGH
+    case 3u: iHash ^= coreUint64(pData[2]) << 16u; FALLTHROUGH
+    case 2u: iHash ^= coreUint64(pData[1]) << 8u;  FALLTHROUGH
+    case 1u: iHash ^= coreUint64(pData[0]);
+             iHash *= m;
+    };
+
+    iHash ^= iHash >> r;
+    iHash *= m;
+    iHash ^= iHash >> r;
 
     return iHash;
 }
