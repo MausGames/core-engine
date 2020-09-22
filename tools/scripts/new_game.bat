778397fb98d5bbd3c9b271ee69a9c895399f91e8
Martin Mauersics <revision@maus-games.at>
2020-09-22 23:57:09 +0200

    updated Visual Studio project structure

--- tools/scripts/new_game.bat
+++ tools/scripts/new_game.bat
@@ -7,7 +7,6 @@ SET PRJ_CUR=%CD%\..\template
 CD ../../..
 SET PRJ_TARGET=%CD%\%PRJ_NAME%\
 
-
 SET /p PRJ_CONFIRM=Create folder ^<%PRJ_TARGET%^> [y/n]: 
 IF NOT %PRJ_CONFIRM:~0,1% == y EXIT
 
@@ -22,43 +21,45 @@ ECHO Renaming...
 
 
 SET TOOL_REPLACE="%PRJ_CUR%\..\scripts\base\replace_text.bat"
-SET TOOL_NAME=CoreApp
+SET TOOL_NAME1=CoreApp
+SET TOOL_NAME2=Core App
+SET TOOL_TEMP=temp_file
 
-SET PATH_NETBEANS=%PRJ_TARGET%\projects\netbeans\nbproject
+SET PATH_CMAKE=%PRJ_TARGET%\projects\cmake
 SET PATH_VISUALSTUDIO=%PRJ_TARGET%\projects\visualstudio
 
+CD "%PATH_CMAKE%"
+CALL RENAME CMakeLists.txt %TOOL_TEMP%
+CALL %TOOL_REPLACE% %TOOL_NAME1% %PRJ_NAME% %TOOL_TEMP% > CMakeLists.txt
+CALL DEL %TOOL_TEMP%
 
-CD "%PATH_NETBEANS%"
-CALL RENAME configurations.xml configurations_temp.xml
-CALL %TOOL_REPLACE% %TOOL_NAME% %PRJ_NAME% configurations_temp.xml > configurations.xml
-CALL DEL configurations_temp.xml
-
-CALL RENAME project.xml project_temp.xml
-CALL %TOOL_REPLACE% %TOOL_NAME% %PRJ_NAME% project_temp.xml > project.xml
-CALL DEL project_temp.xml
-
-
-CD "%PATH_VISUALSTUDIO%\%TOOL_NAME%"
-CALL RENAME %TOOL_NAME%.vcxproj %PRJ_NAME%.vcxproj
-CALL RENAME %TOOL_NAME%.vcxproj.user %PRJ_NAME%.vcxproj.user
+CD "%PATH_VISUALSTUDIO%\configuration"
+CALL RENAME manifest.xml %TOOL_TEMP%
+CALL %TOOL_REPLACE% %TOOL_NAME2% %PRJ_NAME% %TOOL_TEMP% > manifest.xml
+CALL DEL %TOOL_TEMP%
+CALL RENAME project.vcxproj %TOOL_TEMP%
+CALL %TOOL_REPLACE% %TOOL_NAME1% %PRJ_NAME% %TOOL_TEMP% > project.vcxproj
+CALL DEL %TOOL_TEMP%
+CALL RENAME version.rc %TOOL_TEMP%
+CALL %TOOL_REPLACE% %TOOL_NAME2% %PRJ_NAME% %TOOL_TEMP% > version.rc
+CALL DEL %TOOL_TEMP%
 
 CD "%PATH_VISUALSTUDIO%"
-CALL RENAME %TOOL_NAME% %PRJ_NAME%
-CALL %TOOL_REPLACE% %TOOL_NAME% %PRJ_NAME% %TOOL_NAME%.sln > %PRJ_NAME%.sln
-CALL DEL %TOOL_NAME%.sln
-
+CALL %TOOL_REPLACE% %TOOL_NAME1% %PRJ_NAME% %TOOL_NAME1%.sln > %PRJ_NAME%.sln
+CALL DEL %TOOL_NAME1%.sln
 
 CD "%PRJ_TARGET%"
-CALL RENAME .gitignore .gitignore_temp
-CALL %TOOL_REPLACE% %TOOL_NAME% %PRJ_NAME% .gitignore_temp > .gitignore
-CALL DEL .gitignore_temp
+CALL RENAME .gitignore %TOOL_TEMP%
+CALL %TOOL_REPLACE% %TOOL_NAME1% %PRJ_NAME% %TOOL_TEMP% > .gitignore
+CALL DEL %TOOL_TEMP%
 
 CD "executable"
-CALL RENAME %TOOL_NAME%_linux.sh %PRJ_NAME%_linux.sh
-CALL RENAME %TOOL_NAME%_windows.exe %PRJ_NAME%_windows.exe
+CALL RENAME %TOOL_NAME1%_linux.sh %PRJ_NAME%_linux.sh
+CALL RENAME %TOOL_NAME1%_windows.exe %PRJ_NAME%_windows.exe
 
 
 ECHO ^<%PRJ_TARGET%^> created
 %SystemRoot%\explorer.exe "%PRJ_TARGET%"
+
 
 PAUSE
