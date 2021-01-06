7907b70f93a69b0a23084d5f60caf5bbfcbd9941
Martin Mauersics <revision@maus-games.at>
2021-01-07 00:12:40 +0100

    updated all libraries (moved all files to a single location, removed duplicate files, made sure to always use single-digit soname libraries on Linux, removed redundant symlinks)

--- tools/scripts/update_template.bat
+++ tools/scripts/update_template.bat
@@ -7,7 +7,9 @@ IF "%~2" == "defaults" (
     REM not yet implemented
 )
 IF "%~2" == "libraries" (
-    CALL %_EXECUTABLE_% %_PARAMETERS_% "%~dp0..\template\executable\bin" "%~1\executable\bin"
+    CALL %_EXECUTABLE_% %_PARAMETERS_% "%~dp0..\..\libraries\bin\linux\x64\" "%~1\executable\bin\linux\x64\"
+    CALL %_EXECUTABLE_% %_PARAMETERS_% "%~dp0..\..\libraries\bin\windows\x64\dll\" "%~1\executable\bin\windows\x64\"
+    CALL %_EXECUTABLE_% %_PARAMETERS_% "%~dp0..\..\libraries\bin\windows\x86\dll\" "%~1\executable\bin\windows\x86\"
 )
 IF "%~2" == "shaders" (
     CALL %_EXECUTABLE_% %_PARAMETERS_% "%~dp0..\template\executable\data\shaders\global.glsl" "%~1\executable\data\shaders\global.glsl"
--- tools/scripts/update_template.sh
+++ tools/scripts/update_template.sh
@@ -2,13 +2,15 @@
 
 _PATH_="$(dirname $(realpath $0))"
 
-_EXECUTABLE_="cp"
-_PARAMETERS_="--recursive --update --preserve"
+_EXECUTABLE_="rsync"
+_PARAMETERS_="--recursive --update --mkpath --xattrs"
 
 if [ "$2" == "defaults" ]; then
     : # not yet implemented
 elif [ "$2" == "libraries" ]; then
-    $_EXECUTABLE_ $_PARAMETERS_ "$_PATH_/../template/executable/bin" "$1/executable"
+    $_EXECUTABLE_ $_PARAMETERS_ "$_PATH_/../../libraries/bin/linux/x64/" "$1/executable/bin/linux/x64/"
+    $_EXECUTABLE_ $_PARAMETERS_ "$_PATH_/../../libraries/bin/windows/x64/dll/" "$1/executable/bin/windows/x64/"
+    $_EXECUTABLE_ $_PARAMETERS_ "$_PATH_/../../libraries/bin/windows/x86/dll/" "$1/executable/bin/windows/x86/"
 elif [ "$2" == "shaders" ]; then
     $_EXECUTABLE_ $_PARAMETERS_ "$_PATH_/../template/executable/data/shaders/global.glsl" "$1/executable/data/shaders"
     $_EXECUTABLE_ $_PARAMETERS_ "$_PATH_/../template/executable/data/shaders/readme.txt" "$1/executable/data/shaders"
