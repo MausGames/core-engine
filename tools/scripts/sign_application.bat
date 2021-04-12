5d83770759e7b4406aa2d4f3a97030d4eb5c1bcd
Martin Mauersics <revision@maus-games.at>
2021-04-12 21:24:59 +0200

    fixed broken Windows application signing (switched to new compatible timestamp server, switched from SHA1 to SHA256 algorithm (as recommended by Microsoft))

--- tools/scripts/sign_application.bat
+++ tools/scripts/sign_application.bat
@@ -4,6 +4,6 @@ SET VAR_STORE="My"
 SET VAR_CERT=""
 
 SET PATH_SIGN="C:\Program Files (x86)\Windows Kits\10\App Certification Kit\signtool.exe"
-SET PATH_TIME="http://timestamp.verisign.com/scripts/timstamp.dll"
+SET PATH_TIME="http://timestamp.digicert.com"
 
-%PATH_SIGN% sign /s %VAR_STORE% /n %VAR_CERT% /t %PATH_TIME% "%1"
+%PATH_SIGN% sign /s %VAR_STORE% /n %VAR_CERT% /fd SHA256 /tr %PATH_TIME% /td SHA256 "%1"
