0f456900756a00a37ddbc52f01f9a6950fab3273
Martin Mauersics <revision@maus-games.at>
2024-06-26 23:55:49 +0200

    updated the SDL library to v2.30.4 (header only)

--- libraries/include/SDL2/SDL_revision.h
+++ libraries/include/SDL2/SDL_revision.h
@@ -2,7 +2,7 @@
 #define SDL_REVISION_NUMBER 0
 
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-release-2.30.3-0-gfb1497566 (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL-release-2.30.4-0-g92fe3b19c (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL-release-2.30.3-0-gfb1497566"
+#define SDL_REVISION "SDL-release-2.30.4-0-g92fe3b19c"
 #endif
