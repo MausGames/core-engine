9980302eb99e90884fd6215ed2e215679107cf6e
Martin Mauersics <revision@maus-games.at>
2024-11-26 23:48:24 +0100

    implemented new atomic operations to the relaxed atomic class (min, max, exchange)

--- source/utilities/data/coreSpinLock.h
+++ source/utilities/data/coreSpinLock.h
@@ -12,6 +12,7 @@
 
 // TODO 3: implement read-write spinlock (std::shared_mutex)
 // TODO 3: get rid of the mutable mutex
+// TODO 3: use fetch_min and fetch_max with C++26
 
 
 // ****************************************************************
@@ -152,11 +153,16 @@ public:
     FORCE_INLINE T FetchAnd(const T& tValue) {return m_tValue.fetch_and(tValue, std::memory_order::relaxed);}
     FORCE_INLINE T FetchOr (const T& tValue) {return m_tValue.fetch_or (tValue, std::memory_order::relaxed);}
     FORCE_INLINE T FetchXor(const T& tValue) {return m_tValue.fetch_xor(tValue, std::memory_order::relaxed);}
+    FORCE_INLINE T FetchMin(const T& tValue) {T tPrev = m_tValue.load(std::memory_order::relaxed); while((tPrev > tValue) && !m_tValue.compare_exchange_weak(tPrev, tValue, std::memory_order::relaxed)) {} return tPrev;}
+    FORCE_INLINE T FetchMax(const T& tValue) {T tPrev = m_tValue.load(std::memory_order::relaxed); while((tPrev < tValue) && !m_tValue.compare_exchange_weak(tPrev, tValue, std::memory_order::relaxed)) {} return tPrev;}
     FORCE_INLINE T AddFetch(const T& tValue) {return m_tValue.fetch_add(tValue, std::memory_order::relaxed) + tValue;}
     FORCE_INLINE T SubFetch(const T& tValue) {return m_tValue.fetch_sub(tValue, std::memory_order::relaxed) - tValue;}
     FORCE_INLINE T AndFetch(const T& tValue) {return m_tValue.fetch_and(tValue, std::memory_order::relaxed) & tValue;}
     FORCE_INLINE T OrFetch (const T& tValue) {return m_tValue.fetch_or (tValue, std::memory_order::relaxed) | tValue;}
     FORCE_INLINE T XorFetch(const T& tValue) {return m_tValue.fetch_xor(tValue, std::memory_order::relaxed) ^ tValue;}
+    FORCE_INLINE T MinFetch(const T& tValue) {T tPrev = m_tValue.load(std::memory_order::relaxed); while((tPrev > tValue) && !m_tValue.compare_exchange_weak(tPrev, tValue, std::memory_order::relaxed)) {} return MIN(tPrev, tValue);}
+    FORCE_INLINE T MaxFetch(const T& tValue) {T tPrev = m_tValue.load(std::memory_order::relaxed); while((tPrev < tValue) && !m_tValue.compare_exchange_weak(tPrev, tValue, std::memory_order::relaxed)) {} return MAX(tPrev, tValue);}
+    FORCE_INLINE T Exchange(const T& tValue) {return m_tValue.exchange (tValue, std::memory_order::relaxed);}
 
     /* change atomic value with operator */
     FORCE_INLINE T operator += (const T& tValue) {return this->AddFetch(tValue);}   // do not return reference
