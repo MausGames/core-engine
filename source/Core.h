cc44d0f8ff58d8701da38a297384d2ab76d6fd1e
Martin Mauersics <revision@maus-games.at>
2025-08-07 22:01:13 +0200

    Revert "removed support for Ogg Vorbis (and completely replaced it with Ogg Opus)"
    This reverts commit 7e6c5f628fa827cfc3e322a4a2dafbbf768337e5.

--- source/Core.h
+++ source/Core.h
@@ -345,6 +345,9 @@
 #define GLEW_NO_GLU
 #define GLEW_STATIC
 #define AL_DISABLE_NOEXCEPT
+#define STB_VORBIS_HEADER_ONLY
+#define STB_VORBIS_NO_PUSHDATA_API
+#define STB_VORBIS_MAX_CHANNELS 2
 #if defined(_CORE_MSVC_)
     #define ZSTD_DLL_IMPORT 1
 #endif
@@ -363,6 +366,7 @@
 #include <AL/alext.h>
 #include <zstd/zstd.h>
 #include <opus/opusfile.h>
+#include <stb_vorbis.h>
 
 
 // ****************************************************************
