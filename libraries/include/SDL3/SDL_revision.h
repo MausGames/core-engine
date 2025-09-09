0e8717e6e8a9e4e5c4910abba9a544d66c368aca
Martin Mauersics <revision@maus-games.at>
2025-09-09 20:33:44 +0200

    updated the SDL library to v3.2.22 (header only)

--- libraries/include/SDL3/SDL_revision.h
+++ libraries/include/SDL3/SDL_revision.h
@@ -31,9 +31,9 @@
 /* #undef SDL_VENDOR_INFO */
 
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL3-3.2.20-release-3.2.20 (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL3-3.2.22-release-3.2.22 (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL3-3.2.20-release-3.2.20"
+#define SDL_REVISION "SDL3-3.2.22-release-3.2.22"
 #endif
 
 #endif /* SDL_revision_h_ */
