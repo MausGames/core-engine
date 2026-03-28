9fc380c2851ff468ad6ae99fd0bb4f71e7e78ba0
Martin Mauersics <revision@maus-games.at>
2026-03-28 11:49:14 +0100

    changed library setup for Linux and Emscripten (split up libraries into debug and release versions (to make use of debug features and better code stepping), use debug libraries on Linux from the link directory, also adjust some other paths)

--- tools/scripts/update_template.bat
+++ tools/scripts/update_template.bat
@@ -4,7 +4,7 @@ SET _EXECUTABLE_="xcopy"
 SET _PARAMETERS_=/I /K /Y
 
 IF "%~2" == "libraries" (
-    CALL %_EXECUTABLE_% %_PARAMETERS_% "%~dp0..\..\libraries\bin\linux_x86_64\" "%~1\executable\bin\linux_x86_64\"
+    CALL %_EXECUTABLE_% %_PARAMETERS_% "%~dp0..\..\libraries\bin\linux_x86_64\release\" "%~1\executable\bin\linux_x86_64\"
     CALL %_EXECUTABLE_% %_PARAMETERS_% "%~dp0..\..\libraries\bin\windows_x86_32\dll\" "%~1\executable\bin\windows_x86_32\"
     CALL %_EXECUTABLE_% %_PARAMETERS_% "%~dp0..\..\libraries\bin\windows_x86_64\dll\" "%~1\executable\bin\windows_x86_64\"
 )
--- tools/scripts/update_template.sh
+++ tools/scripts/update_template.sh
@@ -6,7 +6,7 @@ _EXECUTABLE_="rsync"
 _PARAMETERS_="--recursive --update --mkpath --xattrs --times --exclude=optional"
 
 if [ "$2" == "libraries" ]; then
-    $_EXECUTABLE_ $_PARAMETERS_ "$_PATH_/../../libraries/bin/linux_x86_64/" "$1/executable/bin/linux_x86_64/"
+    $_EXECUTABLE_ $_PARAMETERS_ "$_PATH_/../../libraries/bin/linux_x86_64/release/" "$1/executable/bin/linux_x86_64/"
     $_EXECUTABLE_ $_PARAMETERS_ "$_PATH_/../../libraries/bin/windows_x86_32/dll/" "$1/executable/bin/windows_x86_32/"
     $_EXECUTABLE_ $_PARAMETERS_ "$_PATH_/../../libraries/bin/windows_x86_64/dll/" "$1/executable/bin/windows_x86_64/"
 elif [ "$2" == "shaders" ]; then
